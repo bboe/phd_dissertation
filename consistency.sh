f1a0e537b792cdf3d546780d8259a1589eac74a5
Bryce Boe <bbzbryce@gmail.com>
2014-07-22 14:18:30 -0700

    Make some consistency changes.

---
+++ consistency.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+grep -iPzoR "feedback\s+and\s+submission" sections/ chap*/
+if [ $? -eq 0 ]; then
+    echo -n  "$(tput setaf 1)"
+    echo "replace instances of 'feedback and submission' with 'feedback and assessment'"
+    echo -n "$(tput setaf 0)"
+fi
+
+grep -iPzoR "large(-|\s+)scale" sections/ chap*/
+if [ $? -eq 0 ]; then
+    echo -n  "$(tput setaf 1)"
+    echo -e "replace instances of 'large-scale' with 'wide-scale'"
+    echo -n  "$(tput setaf 0)"
+fi
+
+grep -iPzoR "real(\s+)time" sections/ chap*/
+if [ $? -eq 0 ]; then
+    echo -n  "$(tput setaf 1)"
+    echo -e "replace instances of 'real time' with 'real-time'"
+    echo -n  "$(tput setaf 0)"
+fi
+
+grep -iPzoR "base(\s+)project" sections/ chap*/
+if [ $? -eq 0 ]; then
+    echo -n  "$(tput setaf 1)"
+    echo -e "replace instances of 'base project' with 'base-project'"
+    echo -n  "$(tput setaf 0)"
+fi
+
+grep -iPR "[^-]project(s?)[^a-z]" sections/ chap*/
+if [ $? -eq 0 ]; then
+    echo -n  "$(tput setaf 1)"
+    echo -e "replace instances of 'project' with 'assignment'"
+    echo -n  "$(tput setaf 0)"
+fi
